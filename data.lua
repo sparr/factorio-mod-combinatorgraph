c08a0328fa2f86c541a71af7d692b6ea38d157fd
Ravager000 <dbombox@gmail.com>
2022-08-16 23:30:38 -0400

    Added a shortcut

--- data.lua
+++ data.lua
@@ -1,12 +1,14 @@
+PATH = "__combinatorgraph__"
 
 data:extend({
   {
     type = "selection-tool",
     name = "combinatorgraph-tool",
-    icon = "__combinatorgraph__/combinatorgraph.png",
+    icon = PATH .. "/graphics/combinatorgraph.png",
     icon_size = 32,
     stack_size = 1,
-    subgroup = "terrain",
+    flags = { "hidden","spawnable","not-stackable","only-in-cursor" },
+    subgroup = "other",
     order = "d[combinatorgraph-tool]-a[plain]",
     selection_color = {r = 0.2, g = 0.8, b = 0.2, a = 0.2},
     alt_selection_color = {r = 0.2, g = 0.8, b = 0.2, a = 0.2},
@@ -18,12 +20,33 @@ data:extend({
   {
     type = "recipe",
     name = "combinatorgraph-tool",
-    enabled = true,
+    enabled = false,
     energy_required = 0.5,
-    ingredients =
-    {
-      {"electronic-circuit", 1}
-    },
+    ingredients ={},
     result = "combinatorgraph-tool"
+  },
+  {
+    type = "shortcut",
+    name = "give-combinatorgraph-tool",
+    order = "d[combinatorgraph-tool]-a[plain]",
+    action = "spawn-item",
+    associated_control_input = "give-combinatorgraph-tool",
+    item_to_spawn = "combinatorgraph-tool",
+    icon =
+    {
+      filename = PATH .. "/graphics/combinatorgraph.png",
+      priority = "extra-high-no-scale",
+      size = 32,
+      mipmap_count = 2,
+      flags = {"gui-icon"}
+    },
+    small_icon =
+    {
+      filename = PATH .. "/graphics/combinatorgraph-x24.png",
+      priority = "extra-high-no-scale",
+      size = 24,
+      mipmap_count = 2,
+      flags = {"gui-icon"}
+    }
   }
 })
